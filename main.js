ca262060ed84ad478309c9887d3457c21aa9d2fe
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2022-12-31 18:25:30 +0600

    Added scroll animation to header

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let mainHEader = document.querySelector(".main-header");
 let mobileMenuHandle = document.querySelector(".mobile-menu-handeler");
 let barIcon = mobileMenuHandle.querySelector("i");
 let navLinks = document.querySelector(".cs-nav-links");
@@ -9,5 +10,16 @@ mobileMenuHandle.addEventListener("click", () => {
   } else {
     barIcon.classList.add("fa-bars-staggered");
     barIcon.classList.remove("fa-xmark");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 25) {
+    mainHEader.classList.remove("py-4");
+    mainHEader.classList.add("py-3");
+    mainHEader.classList.add("active");
+  } else {
+    mainHEader.classList.add("py-4");
+    mainHEader.classList.remove("py-3");
+    mainHEader.classList.remove("active");
   }
 });
